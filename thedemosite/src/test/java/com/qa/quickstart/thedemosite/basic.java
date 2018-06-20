59f4e0df8defc8d7bdc2031ff2a0066a1b67d5a2
Kate <e.genova@live.co.uk>
2018-06-20 09:45:12 +0100

    report dependancy +, demosite report +

--- thedemosite/src/test/java/com/qa/quickstart/thedemosite/basic.java
+++ thedemosite/src/test/java/com/qa/quickstart/thedemosite/basic.java
@@ -5,13 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class basic {
 
+	static ExtentReports extent;
 	ChromeDriver driver;
-
+	
 	@BeforeClass
 	public static void set() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\chromedriver.exe");
+		extent = new ExtentReports("C:\\Users\\Admin\\automated testing\\thedemosite\\reports\\reportDemosite.html", true);
 	}
 	
 	@Before
@@ -19,10 +25,6 @@ public class basic {
 		driver = new ChromeDriver();
 	}
 	
-	@After
-	public void close() {
-		driver.quit();
-	}
 	
 	@Test
 	public void createAndLogIn () {
@@ -43,5 +45,24 @@ public class basic {
 		driver.findElementByName("password").sendKeys(password);
 		driver.findElementByName("FormsButton2").click();
 		assertEquals("**Successful Login**", driver.findElementByXPath(message).getText());
+		
+		
+		ExtentTest test = extent.startTest("Create/Login user");
+		try {
+			test.log(LogStatus.PASS, "Test passed successfully!");
+		} catch (AssertionError e) {
+			test.log(LogStatus.FAIL, "Test failed");
+			fail();
+		} finally {
+			test.log(LogStatus.INFO, "Automated: Create new user, log in as user, confirm user logged in");
+			test.log(LogStatus.INFO, "Current URL: " + driver.getCurrentUrl());
+			extent.endTest(test);
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
+		extent.flush();
 	}
 }
